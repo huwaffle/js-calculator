78bba33f3143d1bf1f17d3b69c44cf6ab6c37519
huwaffle <carlos.alcantara.iii.27@gmail.com>
2026-08-18 23:54:12 +0800

    my-js-calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteDisplay(input){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch(error){
+        display.value = "Invalid Input";
+    }
+    
+}
